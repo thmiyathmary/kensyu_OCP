b219dd90f25bbc692deb7aa4fdc28105c1138bcf
薄井 駿 <s01716@CG2330.local>
2018-04-16 16:25:30 +0900

    優先度の変数の位置を変更

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
 public class Main{
+    List<Shape> priorities = Arrays.asList(Circle, Square);
     public static void main (String args[]){
         Main main = new Main();
         main.testDrawAllShapes();
     }
 
     public void testDrawAllShapes() {
-
         Circle circle = new BigCircle(10, new Point(20,20));
         Square square = new BigSquare(/*side*/ 50, /*topLeft*/ new Point(10,10));
         DrawingTool drawingTool = new DrawingTool();
         List<Shape> shapeList = new ArrayList<Shape>();
-        List<Shape> priorities = Arrays.asList(Circle, Square);
         shapeList.add(circle);
         shapeList.add(square);
         drawingTool.DrawAllShapes(shapeList, priorities);
